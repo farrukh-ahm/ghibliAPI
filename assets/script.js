8aea020dae9c0867544e2f68a589659a2405fcb5
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-07-11 14:00:02 +0000

    Add movie card display

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,36 @@
+
 document.addEventListener("DOMContentLoaded", ()=>{
     getMovies()
 })
 
-
 const getMovies = async()=>{
-    const response = await fetch("https://ghibliapi.vercel.app/films")
+    const response = await fetch("https://ghibliapi.vercel.app/films/")
     const data = await response.json()
     if(response.ok){
-        return console.log(data)
+        console.log(data)
+        movieHandler(data)
     }
     else{
         error
     }
+}
+
+const container = document.querySelector("#container")
+
+const movieHandler = data => {
+
+    const cards = data.map(item=>{
+        return( `<div class="cardContainer"> 
+                <h2>${item.title}</h2>
+                <h4>${item.original_title}</h4>
+                <img src=${item.image} />
+                <p>Writer: ${item.director}</p>
+                <p>Producer: ${item.producer}</p>
+            </div>`)
+    })
+
+    for(let i of cards){
+        container.insertAdjacentHTML('beforeend', i)
+    }
+
 }
